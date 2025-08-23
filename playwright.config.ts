19df462d45bc17080d696ab18a7093e57e751bef
achmadky <achmad.dhikrillah@gmail.com>
2025-08-24 02:55:50 +0700

    add allure

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,10 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   timeout: 30_000,
+  reporter: [
+    ['line'],
+    ['allure-playwright']
+  ],
   use: {
     headless: true,
     baseURL: 'https://bantuan-kita.vercel.app',
